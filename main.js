8cb2cfc706c86a85ae2e6507dffe24815cada07f
phongidoit <93896974+phongidoit@users.noreply.github.com>
2024-06-01 20:35:04 +0700

    follow Cam update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import CannonDebugger from './node_modules/cannon-es-debugger/dist/cannon-es-deb
 
 var gravity = -10;
 
-var scene, clock, world, timeStep=1/60, player, playerBody, controls;
+var scene, clock, world, timeStep=1/60, player, playerBody, controls, followCam;
 var renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled=true;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,12 +23,12 @@ window.camera = camera;
 var keyboard = {};
 var moveSpeed = 10;
 var delta = 0;
-var rotateSpeed = Math.PI / 2 * 1.5;
+var rotateSpeed = Math.PI / 2 * 5;
 
 var startYPosition = 0.3;
 var playerPos = new THREE.Vector3(0, startYPosition, 0);
 var playerRot = new THREE.Vector3(0, 0, 0);
-var jumpVelocity =  10;
+var jumpVelocity =  8;
 
 var isOnGround = true;
 var cameraDeviation = new THREE.Vector3(-5, 4, -10);
@@ -40,25 +40,32 @@ var cannonDebugger = new CannonDebugger( scene, world );
 
 
 function init(){
-    //var gui = new dat.GUI();    
-
     //------environment------
     var map = getMap();  //build terrain here
     clock = new THREE.Clock();
 
     var light = getSpotLight(0.5);
-    var ampLight = getAmbientLight(0.5); 
-    
+    var ampLight = getAmbientLight(0.5);     
 
     player = new THREE.Mesh(
 		new RoundedBoxGeometry( 1.0, 2.0, 1.0, 10, 0.5 ),
-		new THREE.MeshStandardMaterial()
+		[new THREE.MeshStandardMaterial({color: 'lightgrey'}),
+        new THREE.MeshStandardMaterial({color: 'green'}),
+        new THREE.MeshStandardMaterial({color: 'blue'}),
+        new THREE.MeshStandardMaterial({color: 'yellow'}),
+        new THREE.MeshStandardMaterial({color: 'pink'}),
+        new THREE.MeshStandardMaterial({color: 'red'})
+        ]
 	);
     player.geometry.translate(0, -0.5, 0);
     player.capsuleInfo = {
         radius: 0.5, 
         segment: new THREE.Line3(new THREE.Vector3(), new THREE.Vector3(0, 0.0, 0))
     };
+    followCam = getSphere(0.4);
+    followCam.position.z = 8;
+    followCam.position.y = 2;
+    player.add(followCam); 
     player.castShadow = true;
     scene.add(player);
 
@@ -72,31 +79,30 @@ function init(){
     
     camera.position.x = 3; camera.position.y = 6; camera.position.z = 10;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxPolarAngle = Math.PI*7.5/16;
 
-    controls = new OrbitControls(camera, renderer.domElement)
     //controls.update();
-
     document.addEventListener('keydown', function(event) {
         keyboard[event.key] = true;
-        freecam=false;
+        if (event.key != " "){
+            freecam=false;
+        }
     });
-
     document.addEventListener('keyup', function(event) {
         keyboard[event.key] = false;
         freecam = true;
     });
-
     document.addEventListener('click', function(event) {
         freecam=true;
     });
-
     return scene;
 }
 
 function initCannon() {
-    world.gravity.set(0,-12,0);
+    world.gravity.set(0,-10,0);
     world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 1;
+    world.solver.iterations = 10;
 
     playerBody = new CANNON.Body({
         mass: 25, // kg
@@ -119,10 +125,17 @@ function initCannon() {
 
 function getPlane(size){
     var geometry = new THREE.PlaneGeometry(size, size);
+
+    var textureLoader = new THREE.TextureLoader();
     var material = new THREE.MeshBasicMaterial({
         color: 0xf6d7b0,
         side: THREE.DoubleSide
     });
+    material.map = textureLoader.load('./source/sand_texture.jpg');
+    material.map.wrapS = THREE.RepeatWrapping;
+    material.map.wrapT = THREE.RepeatWrapping;
+    material.map.repeat.set(6,6);
+    //material.bumpMap = textureLoader.load('./source/sand_texture.jpg');
 
     var mesh = new THREE.Mesh(
         geometry,
@@ -162,6 +175,7 @@ function getSphere(r){
 
 function getMap(){
     var map = new THREE.Object3D();
+    
     var plane = getPlane(60);
     plane.rotation.x = -Math.PI/2;
 
@@ -183,8 +197,7 @@ function getMap(){
             gltf.scene; // THREE.Group
             gltf.scenes; // Array<THREE.Group>
             gltf.cameras; // Array<THREE.Camera>
-            gltf.asset; // Object
-             
+            gltf.asset; // Object     
         }
     );
     return map;
@@ -200,38 +213,39 @@ function update(renderer, scene, camera, controls, player){
 
     requestAnimationFrame(function(){
         update(renderer, scene, camera, controls, player);
+        //console.log(followCam.matrixWorld);
+        camera.position.setFromMatrixPosition(followCam.matrixWorld);
+        camera.lookAt(player.position);
     })
     delta = clock.getDelta();
+    
+    controls.update();
     handleKeyboardInput(delta, camera, player);
 
-    if (!freecam){
-        if (!updatedTemp)
-        {    
-            tempPos = player.position.clone();
-            tempPos.add(cameraDeviation);
-        }
-        else{
-            //console.log('here');
-            cameraDeviation.set(
-                camera.position.x-player.position.x,
-                camera.position.y-player.position.y,
-                camera.position.z-player.position.z,
+    // if (!freecam){
+    //     if (!updatedTemp)
+    //     {    
+    //         tempPos = player.position.clone();
+    //         tempPos.add(cameraDeviation);
+    //     }
+    //     else{
+    //         //console.log('here');
+    //         cameraDeviation.set(
+    //             camera.position.x-player.position.x,
+    //             camera.position.y-player.position.y,
+    //             camera.position.z-player.position.z,
+    //         );
+    //     }
+    //     //console.log(tempPos);
+    //     camera.position.set(tempPos.x, tempPos.y, tempPos.z);
+    //     controls.target.set(player.position.x, player.position.y, player.position.z);
+    //     updatedTemp = false;
+    // }
+    // else{
+    //     tempPos = camera.position;
+    //     updatedTemp= true;
+    // }
 
-            );
-        }
-        //console.log(tempPos);
-        camera.position.set(tempPos.x, tempPos.y, tempPos.z);
-        controls.target.set(player.position.x, player.position.y, player.position.z);
-        updatedTemp = false;
-    }
-    else{
-        tempPos = camera.position;
-        updatedTemp= true;
-    }
-    
-    camera.lookAt(player.position);
-    controls.update();
-    //console.log(freecam, updatedTemp);
 }
 
 function updatePhysics() {
@@ -240,43 +254,61 @@ function updatePhysics() {
 
     // Copy coordinates from Cannon.js to Three.js
     player.position.copy(playerBody.position);
-    player.position.y += 0.15;
+    player.position.y += 0.12;
     player.quaternion.copy(playerBody.quaternion);
 }
 
+function RoundNum(num)
+{
+    return Math.round(num*100)/100;
+}
+
+function AnyInput(keyboard){
+    var inputrange=['W', 'w', 'S', 's', 'D', 'd', 'A', 'a'];
+    for (let i=0; i<8; i++){
+        if (keyboard[inputrange[i]]){
+            return true;
+        }
+    }
+    return false;
+}
 
 function handleKeyboardInput(delta, camera, player) {
-    const direction = new THREE.Vector3();
-    player.getWorldDirection(direction);
-    //var tempPos = player.copy();
-    //console.log(tempPos)
-    if (keyboard['W'] || keyboard['w']) {
-        player.position.add(direction.multiplyScalar(-moveSpeed * delta));
-        playerBody.position.x = player.position.x;
-        playerBody.position.z = player.position.z;
-    }
-    if (keyboard['S']|| keyboard['s']) {
-        player.position.add(direction.multiplyScalar(moveSpeed * delta));
-        playerBody.position.x = player.position.x;
-        playerBody.position.z = player.position.z;
-    }
-    if (keyboard['A']|| keyboard['a']) {
-        playerRot.y += rotateSpeed * delta;
-    }
-    if (keyboard['D']|| keyboard['d']) {
-        playerRot.y -= rotateSpeed * delta;
-    }
+
     if (keyboard[" "]){
         if (player.position.y <= startYPosition+0.1){
             playerBody.velocity.y = jumpVelocity;
         }
     }
+    if (AnyInput(keyboard)){
+        const direction = new THREE.Vector3();
+        player.getWorldDirection(direction);
 
+        if (keyboard['W'] || keyboard['w']) {
+            player.position.add(direction.multiplyScalar(-moveSpeed * delta));
+            playerBody.position.x = player.position.x;
+            playerBody.position.z = player.position.z;
+        }
+        if (keyboard['S']|| keyboard['s']) {
+            player.position.add(direction.multiplyScalar(moveSpeed * delta));
+            playerBody.position.x = player.position.x;
+            playerBody.position.z = player.position.z;
+        }
+        if (keyboard['A']|| keyboard['a']) {
+            playerRot.y += rotateSpeed * delta;
+        }
+        if (keyboard['D']|| keyboard['d']) {
+            playerRot.y -= rotateSpeed * delta;
+        }        
+       player.rotation.setFromVector3(playerRot);
+
+        //player.position.add(direction.multiplyScalar(-moveSpeed * delta));
+        playerBody.position.x = player.position.x;
+        playerBody.position.z = player.position.z;
+    } 
     
-    player.rotation.setFromVector3(playerRot);
-    //playerBody.quaternion.y = player.rotation.y;
     playerBody.quaternion.copy(player.quaternion);
-
+    
 }
 
 
